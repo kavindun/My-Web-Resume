45c150046e12a3dd91efe1cf1ca6a6ba64ad2262
kavindun <kavindujd1995@gmail.com>
2020-12-07 11:11:12 +0530

    add php mail system

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -45,18 +45,39 @@
   }
   
   if(($emailErr=="")and($nameErr=="")){
-    $msg = "<h3>$name</h3><br><h3>Email : $email</h3><br><h3>$subject<h3><br><p>$message</p>";
+    //$msg = "<h3>$name</h3><br><h3>Email : $email</h3><br><h3>$subject<h3><br><p>$message</p>";
    // echo $msg;
   
     // use wordwrap() if lines are longer than 70 characters
-    $msg = wordwrap($msg,70);
+    //$msg = wordwrap($msg,70);
     
     // send email
-    mail("kavindujd1995@gmail.com","My subject",$msg);
-    echo "<h3>Sent</h3>"
-    
+    //mail("kavindujd1995@gmail.com","My subject",$msg);
+    //echo "<h3>Sent</h3>"
+    require 'phpmailer/PHPMailerAutoload.php';
+    $mail = new PHPMailer();
+
+    $mail->Host='smtp.gmail.com';
+    $mail->Port=587;
+    $mail->SMTPAuth=true;
+    $mail->SMTPSecure='tls';
+    $mail->Username='cvmessage0@gmail.com';
+    $mail->Password='kn//pcpass283';
+
+    $mail->setFrom($email);
+    $mail->addAddress('kavindujd1995@gmail.com');
+
+    $mail->isHTML(true);
+    $mail->Subject='From Submission : '.$subject;
+    $mail->Body='<h1>Name : '.$name.'<br>Email : '.$email.'<br>Subject : '.$subject.'<br>Message : '.$message.'</h1>'
+    if(!$mail->send()){
+      echo "Something went wrong.Please try again."
+    }
+    else{
+      echo "Thanks ".$name."for contacting me. I will get back to you soon!";
+    }
   }
   else{
-    echo "<h3>invalid inputs.</h3>";
+    echo "Invalid inputs.Please try again.";
   }
 ?>
